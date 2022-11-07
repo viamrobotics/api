1417295121193f9ee9c7980acc56a1a9bbb1eac4
Maxim Pertsov <maxim@viam.com>
2022-11-07 17:15:21 -0500

    update setup to install go 1.19

--- etc/setup.sh
+++ etc/setup.sh
@@ -154,7 +154,7 @@ do_brew(){
 
 	# pinned
 	brew "gcc@11"
-	brew "go@1.18"
+	brew "go@1.19"
 	brew "node@18"
 	brew "protobuf@3"
 
@@ -165,7 +165,7 @@ do_brew(){
 	fi
 
 	brew unlink "gcc" "go" "node" "protobuf"
-	brew link --overwrite "gcc@11" "go@1.18" "node@18" "protobuf@3" || exit 1
+	brew link --overwrite "gcc@11" "go@1.19" "node@18" "protobuf@3" || exit 1
 
 	echo "Brew installed software versions..."
 	brew list --version
