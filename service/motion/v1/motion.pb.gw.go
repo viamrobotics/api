43cdf243099dfcd071c7c384dbbe54f720ba76ae
Cheuk Tse <cheuk@viam.com>
2022-11-07 14:27:02 -0500

    fix motion name

--- service/motion/v1/motion.pb.gw.go
+++ service/motion/v1/motion.pb.gw.go
@@ -278,7 +278,7 @@ func RegisterMotionServiceHandlerServer(ctx context.Context, mux *runtime.ServeM
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
-		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/viam.service.motion.v1.MotionService/MoveSingleComponent", runtime.WithHTTPPathPattern("/viam/api/v1/service/motion/{name}/movesinglecomponent"))
+		ctx, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/viam.service.motion.v1.MotionService/MoveSingleComponent", runtime.WithHTTPPathPattern("/viam/api/v1/service/motion/{name}/move_single_component"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -386,7 +386,7 @@ func RegisterMotionServiceHandlerClient(ctx context.Context, mux *runtime.ServeM
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
-		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/viam.service.motion.v1.MotionService/MoveSingleComponent", runtime.WithHTTPPathPattern("/viam/api/v1/service/motion/{name}/movesinglecomponent"))
+		ctx, err = runtime.AnnotateContext(ctx, mux, req, "/viam.service.motion.v1.MotionService/MoveSingleComponent", runtime.WithHTTPPathPattern("/viam/api/v1/service/motion/{name}/move_single_component"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -429,7 +429,7 @@ func RegisterMotionServiceHandlerClient(ctx context.Context, mux *runtime.ServeM
 var (
 	pattern_MotionService_Move_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4, 1, 0, 4, 1, 5, 5, 2, 6}, []string{"viam", "api", "v1", "service", "motion", "name", "move"}, ""))
 
-	pattern_MotionService_MoveSingleComponent_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4, 1, 0, 4, 1, 5, 5, 2, 6}, []string{"viam", "api", "v1", "service", "motion", "name", "movesinglecomponent"}, ""))
+	pattern_MotionService_MoveSingleComponent_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4, 1, 0, 4, 1, 5, 5, 2, 6}, []string{"viam", "api", "v1", "service", "motion", "name", "move_single_component"}, ""))
 
 	pattern_MotionService_GetPose_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4, 1, 0, 4, 1, 5, 5, 2, 6}, []string{"viam", "api", "v1", "service", "motion", "name", "pose"}, ""))
 )
