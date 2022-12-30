7e0adba286652f80bfd4ecf2ebfe2c7df0c78f72
PStrutz <patricia@4strutz.de>
2022-12-30 11:30:48 -0500

    [DATA-975] Add proto definition for MLTrainingService (#143)

---
+++ gen/js/app/mltraining/ml_training_pb_service.js
@@ -0,0 +1,101 @@
+// package: viam.app.mltraining.v1
+// file: app/mltraining/ml_training.proto
+
+var app_mltraining_ml_training_pb = require("../../app/mltraining/ml_training_pb");
+var grpc = require("@improbable-eng/grpc-web").grpc;
+
+var MLTrainingService = (function () {
+  function MLTrainingService() {}
+  MLTrainingService.serviceName = "viam.app.mltraining.v1.MLTrainingService";
+  return MLTrainingService;
+}());
+
+MLTrainingService.SubmitTrainingJob = {
+  methodName: "SubmitTrainingJob",
+  service: MLTrainingService,
+  requestStream: false,
+  responseStream: false,
+  requestType: app_mltraining_ml_training_pb.SubmitTrainingJobRequest,
+  responseType: app_mltraining_ml_training_pb.SubmitTrainingJobResponse
+};
+
+MLTrainingService.GetTrainingJob = {
+  methodName: "GetTrainingJob",
+  service: MLTrainingService,
+  requestStream: false,
+  responseStream: false,
+  requestType: app_mltraining_ml_training_pb.GetTrainingJobRequest,
+  responseType: app_mltraining_ml_training_pb.GetTrainingJobResponse
+};
+
+exports.MLTrainingService = MLTrainingService;
+
+function MLTrainingServiceClient(serviceHost, options) {
+  this.serviceHost = serviceHost;
+  this.options = options || {};
+}
+
+MLTrainingServiceClient.prototype.submitTrainingJob = function submitTrainingJob(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(MLTrainingService.SubmitTrainingJob, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+MLTrainingServiceClient.prototype.getTrainingJob = function getTrainingJob(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(MLTrainingService.GetTrainingJob, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+exports.MLTrainingServiceClient = MLTrainingServiceClient;
+
