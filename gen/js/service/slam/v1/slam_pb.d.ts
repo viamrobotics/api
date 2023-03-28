44b70ae56a77338944363fcb6f10b562a7b56496
PStrutz <patricia@4strutz.de>
2023-03-28 15:59:58 -0400

    RSDK-2488-add-final-slam-api-methods (#226)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- gen/js/service/slam/v1/slam_pb.d.ts
+++ gen/js/service/slam/v1/slam_pb.d.ts
@@ -6,6 +6,142 @@ import * as common_v1_common_pb from "../../../common/v1/common_pb";
 import * as google_api_annotations_pb from "../../../google/api/annotations_pb";
 import * as google_protobuf_struct_pb from "google-protobuf/google/protobuf/struct_pb";
 
+export class GetPositionRequest extends jspb.Message {
+  getName(): string;
+  setName(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): GetPositionRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: GetPositionRequest): GetPositionRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: GetPositionRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): GetPositionRequest;
+  static deserializeBinaryFromReader(message: GetPositionRequest, reader: jspb.BinaryReader): GetPositionRequest;
+}
+
+export namespace GetPositionRequest {
+  export type AsObject = {
+    name: string,
+  }
+}
+
+export class GetPositionResponse extends jspb.Message {
+  hasPose(): boolean;
+  clearPose(): void;
+  getPose(): common_v1_common_pb.Pose | undefined;
+  setPose(value?: common_v1_common_pb.Pose): void;
+
+  getComponentReference(): string;
+  setComponentReference(value: string): void;
+
+  hasExtra(): boolean;
+  clearExtra(): void;
+  getExtra(): google_protobuf_struct_pb.Struct | undefined;
+  setExtra(value?: google_protobuf_struct_pb.Struct): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): GetPositionResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: GetPositionResponse): GetPositionResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: GetPositionResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): GetPositionResponse;
+  static deserializeBinaryFromReader(message: GetPositionResponse, reader: jspb.BinaryReader): GetPositionResponse;
+}
+
+export namespace GetPositionResponse {
+  export type AsObject = {
+    pose?: common_v1_common_pb.Pose.AsObject,
+    componentReference: string,
+    extra?: google_protobuf_struct_pb.Struct.AsObject,
+  }
+}
+
+export class GetPointCloudMapRequest extends jspb.Message {
+  getName(): string;
+  setName(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): GetPointCloudMapRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: GetPointCloudMapRequest): GetPointCloudMapRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: GetPointCloudMapRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): GetPointCloudMapRequest;
+  static deserializeBinaryFromReader(message: GetPointCloudMapRequest, reader: jspb.BinaryReader): GetPointCloudMapRequest;
+}
+
+export namespace GetPointCloudMapRequest {
+  export type AsObject = {
+    name: string,
+  }
+}
+
+export class GetPointCloudMapResponse extends jspb.Message {
+  getPointCloudPcdChunk(): Uint8Array | string;
+  getPointCloudPcdChunk_asU8(): Uint8Array;
+  getPointCloudPcdChunk_asB64(): string;
+  setPointCloudPcdChunk(value: Uint8Array | string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): GetPointCloudMapResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: GetPointCloudMapResponse): GetPointCloudMapResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: GetPointCloudMapResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): GetPointCloudMapResponse;
+  static deserializeBinaryFromReader(message: GetPointCloudMapResponse, reader: jspb.BinaryReader): GetPointCloudMapResponse;
+}
+
+export namespace GetPointCloudMapResponse {
+  export type AsObject = {
+    pointCloudPcdChunk: Uint8Array | string,
+  }
+}
+
+export class GetInternalStateRequest extends jspb.Message {
+  getName(): string;
+  setName(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): GetInternalStateRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: GetInternalStateRequest): GetInternalStateRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: GetInternalStateRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): GetInternalStateRequest;
+  static deserializeBinaryFromReader(message: GetInternalStateRequest, reader: jspb.BinaryReader): GetInternalStateRequest;
+}
+
+export namespace GetInternalStateRequest {
+  export type AsObject = {
+    name: string,
+  }
+}
+
+export class GetInternalStateResponse extends jspb.Message {
+  getInternalStateChunk(): Uint8Array | string;
+  getInternalStateChunk_asU8(): Uint8Array;
+  getInternalStateChunk_asB64(): string;
+  setInternalStateChunk(value: Uint8Array | string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): GetInternalStateResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: GetInternalStateResponse): GetInternalStateResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: GetInternalStateResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): GetInternalStateResponse;
+  static deserializeBinaryFromReader(message: GetInternalStateResponse, reader: jspb.BinaryReader): GetInternalStateResponse;
+}
+
+export namespace GetInternalStateResponse {
+  export type AsObject = {
+    internalStateChunk: Uint8Array | string,
+  }
+}
+
 export class GetPositionNewRequest extends jspb.Message {
   getName(): string;
   setName(value: string): void;
--- gen/js/service/slam/v1/slam_pb_service.js
+++ gen/js/service/slam/v1/slam_pb_service.js
@@ -11,6 +11,33 @@ var SLAMService = (function () {
   return SLAMService;
 }());
 
+SLAMService.GetPosition = {
+  methodName: "GetPosition",
+  service: SLAMService,
+  requestStream: false,
+  responseStream: false,
+  requestType: service_slam_v1_slam_pb.GetPositionRequest,
+  responseType: service_slam_v1_slam_pb.GetPositionResponse
+};
+
+SLAMService.GetPointCloudMap = {
+  methodName: "GetPointCloudMap",
+  service: SLAMService,
+  requestStream: false,
+  responseStream: true,
+  requestType: service_slam_v1_slam_pb.GetPointCloudMapRequest,
+  responseType: service_slam_v1_slam_pb.GetPointCloudMapResponse
+};
+
+SLAMService.GetInternalState = {
+  methodName: "GetInternalState",
+  service: SLAMService,
+  requestStream: false,
+  responseStream: true,
+  requestType: service_slam_v1_slam_pb.GetInternalStateRequest,
+  responseType: service_slam_v1_slam_pb.GetInternalStateResponse
+};
+
 SLAMService.GetPositionNew = {
   methodName: "GetPositionNew",
   service: SLAMService,
@@ -53,6 +80,115 @@ function SLAMServiceClient(serviceHost, options) {
   this.serviceHost = serviceHost;
   this.options = options || {};
 }
+
+SLAMServiceClient.prototype.getPosition = function getPosition(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(SLAMService.GetPosition, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+SLAMServiceClient.prototype.getPointCloudMap = function getPointCloudMap(requestMessage, metadata) {
+  var listeners = {
+    data: [],
+    end: [],
+    status: []
+  };
+  var client = grpc.invoke(SLAMService.GetPointCloudMap, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onMessage: function (responseMessage) {
+      listeners.data.forEach(function (handler) {
+        handler(responseMessage);
+      });
+    },
+    onEnd: function (status, statusMessage, trailers) {
+      listeners.status.forEach(function (handler) {
+        handler({ code: status, details: statusMessage, metadata: trailers });
+      });
+      listeners.end.forEach(function (handler) {
+        handler({ code: status, details: statusMessage, metadata: trailers });
+      });
+      listeners = null;
+    }
+  });
+  return {
+    on: function (type, handler) {
+      listeners[type].push(handler);
+      return this;
+    },
+    cancel: function () {
+      listeners = null;
+      client.close();
+    }
+  };
+};
+
+SLAMServiceClient.prototype.getInternalState = function getInternalState(requestMessage, metadata) {
+  var listeners = {
+    data: [],
+    end: [],
+    status: []
+  };
+  var client = grpc.invoke(SLAMService.GetInternalState, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onMessage: function (responseMessage) {
+      listeners.data.forEach(function (handler) {
+        handler(responseMessage);
+      });
+    },
+    onEnd: function (status, statusMessage, trailers) {
+      listeners.status.forEach(function (handler) {
+        handler({ code: status, details: statusMessage, metadata: trailers });
+      });
+      listeners.end.forEach(function (handler) {
+        handler({ code: status, details: statusMessage, metadata: trailers });
+      });
+      listeners = null;
+    }
+  });
+  return {
+    on: function (type, handler) {
+      listeners[type].push(handler);
+      return this;
+    },
+    cancel: function () {
+      listeners = null;
+      client.close();
+    }
+  };
+};
 
 SLAMServiceClient.prototype.getPositionNew = function getPositionNew(requestMessage, metadata, callback) {
   if (arguments.length === 2) {
