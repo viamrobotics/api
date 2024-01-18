56f5cb47cf60eabc9b96bc0ad22a2a2c239ba058
Benjamin Rewis <32186188+benjirewis@users.noreply.github.com>
2024-01-18 12:17:21 -0500

    RSDK-6378 Add proto file for generic service (#430)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ gen/js/service/generic/v1/generic_pb_service.d.ts
@@ -0,0 +1,64 @@
+// package: viam.service.generic.v1
+// file: service/generic/v1/generic.proto
+
+import * as service_generic_v1_generic_pb from "../../../service/generic/v1/generic_pb";
+import * as common_v1_common_pb from "../../../common/v1/common_pb";
+import {grpc} from "@improbable-eng/grpc-web";
+
+type GenericServiceDoCommand = {
+  readonly methodName: string;
+  readonly service: typeof GenericService;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof common_v1_common_pb.DoCommandRequest;
+  readonly responseType: typeof common_v1_common_pb.DoCommandResponse;
+};
+
+export class GenericService {
+  static readonly serviceName: string;
+  static readonly DoCommand: GenericServiceDoCommand;
+}
+
+export type ServiceError = { message: string, code: number; metadata: grpc.Metadata }
+export type Status = { details: string, code: number; metadata: grpc.Metadata }
+
+interface UnaryResponse {
+  cancel(): void;
+}
+interface ResponseStream<T> {
+  cancel(): void;
+  on(type: 'data', handler: (message: T) => void): ResponseStream<T>;
+  on(type: 'end', handler: (status?: Status) => void): ResponseStream<T>;
+  on(type: 'status', handler: (status: Status) => void): ResponseStream<T>;
+}
+interface RequestStream<T> {
+  write(message: T): RequestStream<T>;
+  end(): void;
+  cancel(): void;
+  on(type: 'end', handler: (status?: Status) => void): RequestStream<T>;
+  on(type: 'status', handler: (status: Status) => void): RequestStream<T>;
+}
+interface BidirectionalStream<ReqT, ResT> {
+  write(message: ReqT): BidirectionalStream<ReqT, ResT>;
+  end(): void;
+  cancel(): void;
+  on(type: 'data', handler: (message: ResT) => void): BidirectionalStream<ReqT, ResT>;
+  on(type: 'end', handler: (status?: Status) => void): BidirectionalStream<ReqT, ResT>;
+  on(type: 'status', handler: (status: Status) => void): BidirectionalStream<ReqT, ResT>;
+}
+
+export class GenericServiceClient {
+  readonly serviceHost: string;
+
+  constructor(serviceHost: string, options?: grpc.RpcOptions);
+  doCommand(
+    requestMessage: common_v1_common_pb.DoCommandRequest,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: common_v1_common_pb.DoCommandResponse|null) => void
+  ): UnaryResponse;
+  doCommand(
+    requestMessage: common_v1_common_pb.DoCommandRequest,
+    callback: (error: ServiceError|null, responseMessage: common_v1_common_pb.DoCommandResponse|null) => void
+  ): UnaryResponse;
+}
+
