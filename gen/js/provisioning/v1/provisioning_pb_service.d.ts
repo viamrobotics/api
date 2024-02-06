811d84e90e803beb7a48003489480f97668031f4
Ale Paredes <1709578+ale7714@users.noreply.github.com>
2024-02-06 16:46:57 -0500

    [APP-3742] Provisioning proto (#438)
    Co-authored-by: Ale Paredes <aleu@viam.com>
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ gen/js/provisioning/v1/provisioning_pb_service.d.ts
@@ -0,0 +1,120 @@
+// package: viam.provisioning.v1
+// file: provisioning/v1/provisioning.proto
+
+import * as provisioning_v1_provisioning_pb from "../../provisioning/v1/provisioning_pb";
+import {grpc} from "@improbable-eng/grpc-web";
+
+type ProvisioningServiceGetSmartMachineStatus = {
+  readonly methodName: string;
+  readonly service: typeof ProvisioningService;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof provisioning_v1_provisioning_pb.GetSmartMachineStatusRequest;
+  readonly responseType: typeof provisioning_v1_provisioning_pb.GetSmartMachineStatusResponse;
+};
+
+type ProvisioningServiceSetNetworkCredentials = {
+  readonly methodName: string;
+  readonly service: typeof ProvisioningService;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof provisioning_v1_provisioning_pb.SetNetworkCredentialsRequest;
+  readonly responseType: typeof provisioning_v1_provisioning_pb.SetNetworkCredentialsResponse;
+};
+
+type ProvisioningServiceSetSmartMachineCredentials = {
+  readonly methodName: string;
+  readonly service: typeof ProvisioningService;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof provisioning_v1_provisioning_pb.SetSmartMachineCredentialsRequest;
+  readonly responseType: typeof provisioning_v1_provisioning_pb.SetSmartMachineCredentialsResponse;
+};
+
+type ProvisioningServiceGetNetworkList = {
+  readonly methodName: string;
+  readonly service: typeof ProvisioningService;
+  readonly requestStream: false;
+  readonly responseStream: false;
+  readonly requestType: typeof provisioning_v1_provisioning_pb.GetNetworkListRequest;
+  readonly responseType: typeof provisioning_v1_provisioning_pb.GetNetworkListResponse;
+};
+
+export class ProvisioningService {
+  static readonly serviceName: string;
+  static readonly GetSmartMachineStatus: ProvisioningServiceGetSmartMachineStatus;
+  static readonly SetNetworkCredentials: ProvisioningServiceSetNetworkCredentials;
+  static readonly SetSmartMachineCredentials: ProvisioningServiceSetSmartMachineCredentials;
+  static readonly GetNetworkList: ProvisioningServiceGetNetworkList;
+}
+
+export type ServiceError = { message: string, code: number; metadata: grpc.Metadata }
+export type Status = { details: string, code: number; metadata: grpc.Metadata }
+
+interface UnaryResponse {
+  cancel(): void;
+}
+interface ResponseStream<T> {
+  cancel(): void;
+  on(type: 'data', handler: (message: T) => void): ResponseStream<T>;
+  on(type: 'end', handler: (status?: Status) => void): ResponseStream<T>;
+  on(type: 'status', handler: (status: Status) => void): ResponseStream<T>;
+}
+interface RequestStream<T> {
+  write(message: T): RequestStream<T>;
+  end(): void;
+  cancel(): void;
+  on(type: 'end', handler: (status?: Status) => void): RequestStream<T>;
+  on(type: 'status', handler: (status: Status) => void): RequestStream<T>;
+}
+interface BidirectionalStream<ReqT, ResT> {
+  write(message: ReqT): BidirectionalStream<ReqT, ResT>;
+  end(): void;
+  cancel(): void;
+  on(type: 'data', handler: (message: ResT) => void): BidirectionalStream<ReqT, ResT>;
+  on(type: 'end', handler: (status?: Status) => void): BidirectionalStream<ReqT, ResT>;
+  on(type: 'status', handler: (status: Status) => void): BidirectionalStream<ReqT, ResT>;
+}
+
+export class ProvisioningServiceClient {
+  readonly serviceHost: string;
+
+  constructor(serviceHost: string, options?: grpc.RpcOptions);
+  getSmartMachineStatus(
+    requestMessage: provisioning_v1_provisioning_pb.GetSmartMachineStatusRequest,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: provisioning_v1_provisioning_pb.GetSmartMachineStatusResponse|null) => void
+  ): UnaryResponse;
+  getSmartMachineStatus(
+    requestMessage: provisioning_v1_provisioning_pb.GetSmartMachineStatusRequest,
+    callback: (error: ServiceError|null, responseMessage: provisioning_v1_provisioning_pb.GetSmartMachineStatusResponse|null) => void
+  ): UnaryResponse;
+  setNetworkCredentials(
+    requestMessage: provisioning_v1_provisioning_pb.SetNetworkCredentialsRequest,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: provisioning_v1_provisioning_pb.SetNetworkCredentialsResponse|null) => void
+  ): UnaryResponse;
+  setNetworkCredentials(
+    requestMessage: provisioning_v1_provisioning_pb.SetNetworkCredentialsRequest,
+    callback: (error: ServiceError|null, responseMessage: provisioning_v1_provisioning_pb.SetNetworkCredentialsResponse|null) => void
+  ): UnaryResponse;
+  setSmartMachineCredentials(
+    requestMessage: provisioning_v1_provisioning_pb.SetSmartMachineCredentialsRequest,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: provisioning_v1_provisioning_pb.SetSmartMachineCredentialsResponse|null) => void
+  ): UnaryResponse;
+  setSmartMachineCredentials(
+    requestMessage: provisioning_v1_provisioning_pb.SetSmartMachineCredentialsRequest,
+    callback: (error: ServiceError|null, responseMessage: provisioning_v1_provisioning_pb.SetSmartMachineCredentialsResponse|null) => void
+  ): UnaryResponse;
+  getNetworkList(
+    requestMessage: provisioning_v1_provisioning_pb.GetNetworkListRequest,
+    metadata: grpc.Metadata,
+    callback: (error: ServiceError|null, responseMessage: provisioning_v1_provisioning_pb.GetNetworkListResponse|null) => void
+  ): UnaryResponse;
+  getNetworkList(
+    requestMessage: provisioning_v1_provisioning_pb.GetNetworkListRequest,
+    callback: (error: ServiceError|null, responseMessage: provisioning_v1_provisioning_pb.GetNetworkListResponse|null) => void
+  ): UnaryResponse;
+}
+
