3771d837d930e940c7dfe7c43918dbab9869010a
randhid <35934754+randhid@users.noreply.github.com>
2024-02-14 12:12:29 -0500

    Protos for latest breaking change did not compile (#451)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- gen/js/app/packages/v1/packages_pb.js
+++ gen/js/app/packages/v1/packages_pb.js
@@ -2741,8 +2741,7 @@ proto.viam.app.packages.v1.PackageType = {
   PACKAGE_TYPE_ARCHIVE: 1,
   PACKAGE_TYPE_ML_MODEL: 2,
   PACKAGE_TYPE_MODULE: 3,
-  PACKAGE_TYPE_SLAM_MAP: 4,
-  PACKAGE_TYPE_BOARD_DEFS: 5
+  PACKAGE_TYPE_SLAM_MAP: 4
 };
 
 goog.object.extend(exports, proto.viam.app.packages.v1);
