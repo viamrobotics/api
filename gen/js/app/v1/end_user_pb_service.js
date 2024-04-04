74154690cafa597f232e4ce9a0a97d314fbfa5e5
Julia Ruddy <julia.ruddy@viam.com>
2024-04-04 11:00:29 -0400

    APP-2985: Protos for endpoint to register fusionauth applications (#460)

--- gen/js/app/v1/end_user_pb_service.js
+++ gen/js/app/v1/end_user_pb_service.js
@@ -28,6 +28,15 @@ EndUserService.AcceptLegal = {
   responseType: app_v1_end_user_pb.AcceptLegalResponse
 };
 
+EndUserService.RegisterAuthApplication = {
+  methodName: "RegisterAuthApplication",
+  service: EndUserService,
+  requestStream: false,
+  responseStream: false,
+  requestType: app_v1_end_user_pb.RegisterAuthApplicationRequest,
+  responseType: app_v1_end_user_pb.RegisterAuthApplicationResponse
+};
+
 exports.EndUserService = EndUserService;
 
 function EndUserServiceClient(serviceHost, options) {
@@ -71,6 +80,37 @@ EndUserServiceClient.prototype.acceptLegal = function acceptLegal(requestMessage
     callback = arguments[1];
   }
   var client = grpc.unary(EndUserService.AcceptLegal, {
+    request: requestMessage,
+    host: this.serviceHost,
+    metadata: metadata,
+    transport: this.options.transport,
+    debug: this.options.debug,
+    onEnd: function (response) {
+      if (callback) {
+        if (response.status !== grpc.Code.OK) {
+          var err = new Error(response.statusMessage);
+          err.code = response.status;
+          err.metadata = response.trailers;
+          callback(err, null);
+        } else {
+          callback(null, response.message);
+        }
+      }
+    }
+  });
+  return {
+    cancel: function () {
+      callback = null;
+      client.close();
+    }
+  };
+};
+
+EndUserServiceClient.prototype.registerAuthApplication = function registerAuthApplication(requestMessage, metadata, callback) {
+  if (arguments.length === 2) {
+    callback = arguments[1];
+  }
+  var client = grpc.unary(EndUserService.RegisterAuthApplication, {
     request: requestMessage,
     host: this.serviceHost,
     metadata: metadata,
