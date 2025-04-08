35536c756840d50ed7060b96b72f06aa5567c74f
Katie Peters <katiepeters@users.noreply.github.com>
2025-04-08 11:35:54 -0400

    DATA-3956 Add protos for enable/disable endpoints (#664)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- app/datapipelines/v1/data_pipelines.pb.gw.go
+++ app/datapipelines/v1/data_pipelines.pb.gw.go
@@ -161,6 +161,58 @@ func local_request_DataPipelinesService_DeleteDataPipeline_0(ctx context.Context
 
 }
 
+func request_DataPipelinesService_EnableDataPipeline_0(ctx context.Context, marshaler runtime.Marshaler, client DataPipelinesServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq EnableDataPipelineRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.EnableDataPipeline(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_DataPipelinesService_EnableDataPipeline_0(ctx context.Context, marshaler runtime.Marshaler, server DataPipelinesServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq EnableDataPipelineRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.EnableDataPipeline(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
+func request_DataPipelinesService_DisableDataPipeline_0(ctx context.Context, marshaler runtime.Marshaler, client DataPipelinesServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq DisableDataPipelineRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.DisableDataPipeline(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_DataPipelinesService_DisableDataPipeline_0(ctx context.Context, marshaler runtime.Marshaler, server DataPipelinesServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq DisableDataPipelineRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.DisableDataPipeline(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterDataPipelinesServiceHandlerServer registers the http handlers for service DataPipelinesService to "mux".
 // UnaryRPC     :call DataPipelinesServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -289,6 +341,56 @@ func RegisterDataPipelinesServiceHandlerServer(ctx context.Context, mux *runtime
 		}
 
 		forward_DataPipelinesService_DeleteDataPipeline_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_DataPipelinesService_EnableDataPipeline_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/viam.app.datapipelines.v1.DataPipelinesService/EnableDataPipeline", runtime.WithHTTPPathPattern("/viam.app.datapipelines.v1.DataPipelinesService/EnableDataPipeline"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_DataPipelinesService_EnableDataPipeline_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_DataPipelinesService_EnableDataPipeline_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_DataPipelinesService_DisableDataPipeline_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/viam.app.datapipelines.v1.DataPipelinesService/DisableDataPipeline", runtime.WithHTTPPathPattern("/viam.app.datapipelines.v1.DataPipelinesService/DisableDataPipeline"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_DataPipelinesService_DisableDataPipeline_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_DataPipelinesService_DisableDataPipeline_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -443,6 +545,50 @@ func RegisterDataPipelinesServiceHandlerClient(ctx context.Context, mux *runtime
 
 	})
 
+	mux.Handle("POST", pattern_DataPipelinesService_EnableDataPipeline_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/viam.app.datapipelines.v1.DataPipelinesService/EnableDataPipeline", runtime.WithHTTPPathPattern("/viam.app.datapipelines.v1.DataPipelinesService/EnableDataPipeline"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_DataPipelinesService_EnableDataPipeline_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_DataPipelinesService_EnableDataPipeline_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_DataPipelinesService_DisableDataPipeline_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/viam.app.datapipelines.v1.DataPipelinesService/DisableDataPipeline", runtime.WithHTTPPathPattern("/viam.app.datapipelines.v1.DataPipelinesService/DisableDataPipeline"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_DataPipelinesService_DisableDataPipeline_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_DataPipelinesService_DisableDataPipeline_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -456,6 +602,10 @@ var (
 	pattern_DataPipelinesService_UpdateDataPipeline_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"viam.app.datapipelines.v1.DataPipelinesService", "UpdateDataPipeline"}, ""))
 
 	pattern_DataPipelinesService_DeleteDataPipeline_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"viam.app.datapipelines.v1.DataPipelinesService", "DeleteDataPipeline"}, ""))
+
+	pattern_DataPipelinesService_EnableDataPipeline_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"viam.app.datapipelines.v1.DataPipelinesService", "EnableDataPipeline"}, ""))
+
+	pattern_DataPipelinesService_DisableDataPipeline_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"viam.app.datapipelines.v1.DataPipelinesService", "DisableDataPipeline"}, ""))
 )
 
 var (
@@ -468,4 +618,8 @@ var (
 	forward_DataPipelinesService_UpdateDataPipeline_0 = runtime.ForwardResponseMessage
 
 	forward_DataPipelinesService_DeleteDataPipeline_0 = runtime.ForwardResponseMessage
+
+	forward_DataPipelinesService_EnableDataPipeline_0 = runtime.ForwardResponseMessage
+
+	forward_DataPipelinesService_DisableDataPipeline_0 = runtime.ForwardResponseMessage
 )
