70f7a9cf9e45c222345d8bb4352c2d62427ce886
Travis Gritter <travis.gritter@gmail.com>
2026-01-26 14:53:47 -0500

    [APP-14672] Adds defaults_fragments to organization proto (#806)
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- gen/js/app/v1/app_pb.d.ts
+++ gen/js/app/v1/app_pb.d.ts
@@ -262,6 +262,11 @@ export class Organization extends jspb.Message {
   getCid(): string;
   setCid(value: string): void;
 
+  hasDefaultFragments(): boolean;
+  clearDefaultFragments(): void;
+  getDefaultFragments(): FragmentImportList | undefined;
+  setDefaultFragments(value?: FragmentImportList): void;
+
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): Organization.AsObject;
   static toObject(includeInstance: boolean, msg: Organization): Organization.AsObject;
@@ -280,6 +285,7 @@ export namespace Organization {
     publicNamespace: string,
     defaultRegion: string,
     cid: string,
+    defaultFragments?: FragmentImportList.AsObject,
   }
 }
 
